a997f32c3eeb6b5aa2aafe3bc94c8fe084df2fc9
binbbaz <abbasabdulsalam@Abbass-MacBook-Pro.local>
2023-02-07 09:04:46 +0400

    Avatar Implementation, Continuous Moving, Continuous Turn and Character Controller, Teleportation

---
+++ Library/Bee/Android/Prj/Mono2x/Gradle/launcher/build/generated/source/buildConfig/release/com/DPWorld/Multiplayer_learning/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.DPWorld.Multiplayer_learning;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.DPWorld.Multiplayer_learning";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.1";
+}
